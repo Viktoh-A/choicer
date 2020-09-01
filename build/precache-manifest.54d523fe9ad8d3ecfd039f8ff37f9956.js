7d3e3bfc1795836e1cf6ba06350fc3307906b90a
Amaechi Victor <30497549+Viktoh-A@users.noreply.github.com>
2020-09-01 11:06:49 +0100

    Add files via upload

---
+++ build/precache-manifest.54d523fe9ad8d3ecfd039f8ff37f9956.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4dd6e25c27927c108c4bc51ab103dbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce53c80e190a731ecb55",
+    "url": "/static/css/main.aa9f9a91.chunk.css"
+  },
+  {
+    "revision": "3cccf54b9c1b7f6b5d8f",
+    "url": "/static/js/2.480c024d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.480c024d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce53c80e190a731ecb55",
+    "url": "/static/js/main.fc4b5418.chunk.js"
+  },
+  {
+    "revision": "d650dba18025899d845c",
+    "url": "/static/js/runtime-main.a66abbec.js"
+  }
+]);
